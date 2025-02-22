fc7a897371a7b95b7ec509edf5e7a3f4e6977910
SwagLord!! <cityguilt@gmail.com>
2025-02-22 16:32:20 +0100

    config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const config = {
   bot_twitter: {
     tracker_timeout: 300000, // 5 min
     run_headless: true,
-    default_browser: "C:/Program Files/Google/Chrome/Application/chrome.exe", // Replace with the path to your browser
+    default_browser: "C:\Program` Files` (x86)\Google\Chrome\Application\chrome.exe", // Replace with the path to your browser
     default_browser_data: "C:/Users/bevan/AppData/Local/Google/Chrome/User Data", // Replace with the path to your browser's user data directory
     accounts: [
       {
